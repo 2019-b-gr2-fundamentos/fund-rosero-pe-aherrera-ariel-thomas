34a6c45c906ecf12f19a5bffd3f84219caaed2ef
Arie27R <52940197+Arie27R@users.noreply.github.com>
2019-10-17 17:42:49 -0500

    Variables Cheveres

---
+++ Mi carpeta/01-variables.js
@@ -0,0 +1,37 @@
+/*
+Nombre: Ariel Rosero
+Fecha de actualizaciòn: 17/10/2019
+Descripciòn: Ejemplos de variables primitivas en JS.
+*/
+//comentario
+/* <- abro el bloque de comentario
+cierro el bloque de comentario ->*/
+
+//Primitivas (Tipos de variables)
+/*
+Int         -> Numero ENTERO                Ej: 8
+char        -> Caracter                     Ej: A
+float       -> Numeros Decimales            Ej: -2.14
+double      -> N.Decimales +++              Ej: 1212.12.......... 
+string      -> Arrreglo de Caracteres       Ej: "abs" o "cde"
+null        -> VACIO                        Ej: null 
+boolean     -> Verdadero / Falso            Ej: True o False
+*/
+// var edad = 19; //
+let edad = 19;    // Mutable -> Cambiar
+//const edad = 19;  // Inmutable -> No cambiar
+const IVA = 12;
+//Iva = 14; //No reasignar Valores
+let sueldo = 0;
+// edad = 30 -> python
+//var edad: Integer = 30; -> kotlin Mutable
+//val edad: Integer = 30; -> kotlin Inmutable
+
+// --> INMUTABLES
+//let IVA = 12;
+//const IVA = 12;
+
+
+
+
+
