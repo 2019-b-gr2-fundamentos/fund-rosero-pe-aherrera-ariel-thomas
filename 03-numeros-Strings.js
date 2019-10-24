85c8eccb154d32bf7425f9564fe8b07595689ee7
Arie27R <52940197+Arie27R@users.noreply.github.com>
2019-10-24 17:57:30 -0500

    Clase Strings

--- 03-numeros-Strings.js
+++ 03-numeros-Strings.js
@@ -23,4 +23,39 @@ console.log("pera" + 2);
 //NumeroUno -> 
 //numerouno
 //numero_uno
-//numerOuno ->  
+//numerOuno -> 
+//STRINGS
+const mensaje = "\"Hola mundo\"";
+const saludo = '\'Adio\s Mundo\'';
+console.log(mensaje + "1\t\t\t 3\n" + saludo);
+//Template strings
+const edad = 30;
+const saludoEdad = `Mi edad es: ${edad}`;
+console.log(saludoEdad);
+const saludoCompleto = `${mensaje}\n${saludo}`;
+console.log(saludoCompleto);
+//Contamos los elementos
+const nombreLongitud = "Ariel Thomàs";
+console.log(nombreLongitud.length);
+//Eliminar espacios al principio y al final
+console.log(nombreLongitud.trim());
+//Cambiar a Mayusculas
+console.log(nombreLongitud.toUpperCase());
+//console.log(nombreLongitud.toLowerCase());
+//console.log(nombreLongitud.replace("e","o"));
+//console.log(nombreLongitud.replace("a","o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.replace("x", "o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.replace("a", "o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.search("z")); //-1
+console.log(nombreLongitud.search("e")); // Si encuentra me dice la posiciòn en la que se encuentra
+console.log(nombreLongitud.slice(0, 5));
+console.log(nombreLongitud.indexOf("a")); //
+console.log(nombreLongitud.indexOf("x")); //
+console.log(nombreLongitud.includes(" ")); // booleano
+console.log(nombreLongitud.includes("x")); // booleano
+console.log(nombreLongitud.endsWith("x")); //booleano
+console.log(nombreLongitud.startsWith("vice")); //booleano
+console.log(nombreLongitud.substring(0, 5)); //
