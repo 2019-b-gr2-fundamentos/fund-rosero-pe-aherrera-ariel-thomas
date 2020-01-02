404f0f3315227d388165380c4df388a2d72fb0ad
Arie27R <52940197+Arie27R@users.noreply.github.com>
2020-01-02 16:35:31 -0500

    Matrices Finalizado

--- Mi carpeta/04-matrices.js
+++ Mi carpeta/04-matrices.js
@@ -12,36 +12,74 @@ n=2, m=3
 var arregloMatriz = [
     [1, 2],
     [3, 4, 5],
-    [6, 7, 8],
+    [6, 7, 8, 6],
     [9],
+    [],
 ];
-function compararMatrices(matrizUno, matrizDos) {
+function compararMatriz(matrizUno, matrizDos) {
+    var esValido = tienenMatricesIgualesDimensiones(matrizUno, matrizDos);
+    if (esValido) {
+        // Comparar los valores
+        return tienenMismosValores(matrizUno, matrizDos);
+    }
+    else {
+        return false;
+    }
+}
+function tienenMismosValores(matrizUno, matrizDos) {
+    var primeraDimension = matrizUno.length;
+    var segundaDimension = matrizUno[0].length;
+    var banderaSonIguales = true;
+    for (var i = 0; i < primeraDimension; i++) {
+        for (var j = 0; j < segundaDimension; j++) {
+            var valorActualM1 = matrizUno[i][j];
+            var valorActualM2 = matrizDos[i][j];
+            if (valorActualM1 != valorActualM2) {
+                banderaSonIguales = false;
+            }
+        }
+    }
+    return banderaSonIguales;
+}
+function tienenMatricesIgualesDimensiones(matrizUno, matrizDos) {
     var matrizUnoPrimeraDimension = obtenerPrimeraDimension(matrizUno);
     var matrizUnoSegundaDimension = obtenerSegundaDimension(matrizUno);
     var matrizDosPrimeraDimension = obtenerPrimeraDimension(matrizDos);
     var matrizDosSegundaDimension = obtenerSegundaDimension(matrizDos);
-    console.log(matrizUnoPrimeraDimension);
-    console.log(matrizUnoSegundaDimension);
-    console.log(matrizDosPrimeraDimension);
-    console.log(matrizDosSegundaDimension);
-    return true;
+    var noHayFalsos = matrizUnoPrimeraDimension != false &&
+        matrizUnoSegundaDimension != false &&
+        matrizDosPrimeraDimension != false &&
+        matrizDosSegundaDimension != false;
+    if (noHayFalsos) {
+        var tienenIgualesDimensiones = matrizUnoPrimeraDimension == matrizDosPrimeraDimension &&
+            matrizUnoSegundaDimension == matrizDosSegundaDimension;
+        if (tienenIgualesDimensiones) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
 }
 function obtenerPrimeraDimension(matrizUno) {
-    // VALIDACIONES = verificar que todos sean los arreglos
-    var esValido = verificarTodosLosElementosDeUnArregloSonArreglos(matrizUno);
+    // VALIDACIONES
+    var esValido = verificarTodosLosElementosDeUnArregloSonArreglo(matrizUno);
     if (esValido) {
-        var primeraDimensionArrelgo = matrizUno.length;
-        return primeraDimensionArrelgo;
+        var primeraDimensionArreglo = matrizUno.length;
+        return primeraDimensionArreglo;
     }
     else {
         return false;
     }
 }
 function obtenerSegundaDimension(matrizUno) {
-    var esValido = verificarTodosLosElementosDeUnArregloSonArreglos(matrizUno);
+    var esValido = verificarTodosLosElementosDeUnArregloSonArreglo(matrizUno);
     if (esValido) {
-        var longitudActualMaxima = 0; // Variable Auxiliar: GUARDAR EL VALOR Y MANTENERLO DURANTE TODA LA ITERACION
-        var longitudActualMinima = -1; // auxiliar 
+        var longitudActualMaxima = 0; // Auxiliar
+        var longitudActualMinima = -1; // Auxiliar
         for (var i = 0; i < matrizUno.length; i++) {
             var elementoActual = matrizUno[i]; // arreglo
             var longitudActual = elementoActual.length; // segunda dimension
@@ -52,7 +90,7 @@ function obtenerSegundaDimension(matrizUno) {
                 longitudActualMinima = longitudActual;
             }
             else {
-                if (longitudActual < longitudActualMinima && longitudActualMinima >= 0) {
+                if (longitudActual < longitudActualMinima) {
                     longitudActualMinima = longitudActual;
                 }
             }
@@ -68,28 +106,30 @@ function obtenerSegundaDimension(matrizUno) {
         return false;
     }
 }
-function verificarTodosLosElementosDeUnArregloSonArreglos(arreglo) {
+function verificarTodosLosElementosDeUnArregloSonArreglo(arreglo) {
     for (var i = 0; i < arreglo.length; i++) {
         var elementoActual = arreglo[i];
-        var esUnArreglo = typeof elementoActual == 'object' && elementoActual.indexOf; // Truty
+        var esUnArreglo = typeof elementoActual == 'object' &&
+            elementoActual.indexOf; // Truty
         if (!esUnArreglo) {
-            console.log('No es un arreglo');
             return false;
         }
     }
     return true;
 }
 function main() {
-    var matrizUno = [
-        [1, 2],
-        [1]
+    var x = [
+        [1, 2, 3],
+        [3, 3, 3]
     ];
-    var matrizDos = [
-        [1, 2],
-        [3, 4]
+    var y = [
+        [1, 2, 3],
+        [3, 3, 3],
     ];
-    compararMatrices(matrizUno, matrizDos);
+    var resultado = compararMatriz(x, y);
+    console.log('Resultado: ', resultado);
 }
+main();
 /* function verificarTamañoDeCadaArregloDentroDelArreglo(matrizUno: number[][]):boolean{
     const esValido = verificarTodosLosElementosDeUnArregloSonArreglos(matrizUno);
     const longitudPrimerArreglo = matrizUno[0].length;
