06d6331e0ed1815ab8f3ea1263518efb7627cdc1
rommel <rommel.masabanda@epn.edu.ec>
2020-01-20 15:49:49 -0500

    Tipos de Programación

--- Mi carpeta/06-Estructuras-datos/02_crud.ts
+++ Mi carpeta/06-Estructuras-datos/02_crud.ts
@@ -2,9 +2,17 @@
 //const prompts = require('prompts');
 
 import * as prompts from 'prompts';
+import { RespuestaNombre } from './Interfaces/respuesta_nombre.interface';
+import { RespuestaEdad } from './Interfaces/respuesta_edad.interface';
 
 function main() {
-    obtenerDatosAnimalPerrito();
+    //npm uninstall -g tsc -> Windows
+    //npm install -g typescript -> Windows
+    //obtenerDatosAnimalPerrito();  //Asincrona
+    obtenerDatosAnimalPerritoSincrono()
+    .then()
+    .catch(); // --> Sincrona / Promesa
+
 }
 
 function obtenerDatosAnimalPerrito(){
@@ -53,3 +61,31 @@ function obtenerDatosAnimalPerrito(){
 }
 
 main();
+
+async function obtenerDatosAnimalPerritoSincrono(){
+    //Paradigmas de Porgramación
+    console.log('Inicio');
+    const preguntas = [ 
+        {
+            type: 'number',
+            name: 'edad',
+            message: 'Puedes darme tu edad?'
+        },
+        {
+            type: 'text',
+            name: 'nombre',
+            message: 'Puedes darme tu nombre?'
+        },
+        {
+            type: 'text',
+            name: 'cedula',
+            message: 'Puedes darme tu cedula?'
+        }
+    
+    ];
+    const respuestaEdad = await prompts(preguntas);
+    console.log('Respuesta',respuestaEdad);
+    console.log('Fin');
+
+}
+
