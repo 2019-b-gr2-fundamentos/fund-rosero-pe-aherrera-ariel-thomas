fd1c1f40aed8877f48da67a10e89621c9dcba120
Arie27R <52940197+Arie27R@users.noreply.github.com>
2020-01-23 16:57:58 -0500

    EjemploDeber

--- Mi carpeta/04-libreria/lib/suma.js
+++ Mi carpeta/04-libreria/lib/suma.js
@@ -1,7 +1,4 @@
 module.exports = function (numUno, numDos){
-    const cateto1 = math.Pow(numUno,2);
-    const cateto2 = math.Pow(numDos,2);
-   return math.Sqrt( cateto1 + cateto2 )
+    return numUno + numDos;
 }
-
 
--- Mi carpeta/07-archivos/01-archivo.ts
+++ Mi carpeta/07-archivos/01-archivo.ts
@@ -1,14 +1,96 @@
 import { leerArchivo } from "./02-leer-archivo";  //Porbar el hambiente 
 import { escribirArchivo } from "./03-escribir-archivo";
+import { Estudiante } from "./interfaces/estudiantes.interface";
+import * as prompts from 'prompts';
 
-function main() {
-    const textoleido = leerArchivo('./ejemplo.txt');
-    const nuevoContenido = 'TENGO HAMBRE :( \n ';
+async function main() {
+
+    let contador = 1;
+    const contenidoArchivo = leerArchivo(
+        './ejemplo.txt'
+
+    );
+    
+    console.log('contenidoArchivo',contenidoArchivo);
+    const arregloCargadoDeArchivo = JSON.parse(contenidoArchivo);   //funcion que trasforma un 
+    const arregloEstudiantes: Estudiante[] = arregloCargadoDeArchivo;
+    const arregloPreguntas = [
+        { 
+            type: 'text',
+            name: 'nombre',
+            message: 'Ingresa tu nombre'
+        }
+    ];
+    const respuestaEstudianteUno = await prompts(arregloPreguntas);
+    
+    const nuevoRegistroUno = {
+        id: contador,
+        nombre: respuestaEstudianteUno.nombre
+    };
+    contador = contador + 1;
+    arregloEstudiantes.push(nuevoRegistroUno);
+
+    const respuestaEstudianteDos = await prompts(arregloPreguntas);
+    
+    const nuevoRegistroDos = {
+        id: contador,
+        nombre: respuestaEstudianteDos.nombre
+    };
+    contador = contador + 1;
+    arregloEstudiantes.push(nuevoRegistroDos);
+    
+    console.log('Cual usuario quieres Editar?');
+    console.log(arregloEstudiantes);
+
+    // OPERADORES!!! -> REEMPLAZAR AL !FOR!
+
+    const idABuscar = await prompts({
+            type: 'number',
+            name: 'id',
+            message: 'Ingresa el ID del registro a Editar'
+    })
+    const indiceEncontrado = arregloEstudiantes.findIndex( // return CONDICION ->
+        function (valorActual, indice, arreglo){
+            return valorActual.id == idABuscar.id; // Nos devuelve el INDICE
+        } // Si encuentra nos devuelve el indice
+          // No encuentra
+    )
+    console.log('Indice encontrado:', indiceEncontrado);
+    const nombreAEditar = await prompts({
+        type: 'text',
+        name: 'nombre',
+        message: 'Ingresa el nuevo nombre'
+    })
+    arregloEstudiantes[indiceEncontrado].nombre = nombreAEditar.nombre;
+    console.log(arregloEstudiantes);
+
+    const buscar = await prompts({
+        type: 'text',
+        name: 'nombre',
+        message: 'Buscar por ID o por NOMBRE'
+    });
+    const estudianteEncontrado = arregloEstudiantes
+            .find( // return CONDICION
+                function(valorActual){
+                    return valorActual.nombre == buscar.nombre;
+                }
+            );
+    console.log(estudianteEncontrado);
+
+    /*
+    const textoLeido = leerArchivo('./ejemplo.txt');
+    const nuevoContenido = 'TENGO HAMBRE :( \n';
     escribirArchivo('./ejemplo.txt', '');
-    console.log(textoleido + nuevoContenido);
+    console.log(textoLeido + nuevoContenido);
+    */
 }
 
-main();
+main().then().catch();
+
 
 //Guardar algo dentro de un archivo y utilizar algo dentro del programa.
+//const textoleido = leerArchivo('./ejemplo.txt');
+    //const nuevoContenido = 'TENGO HAMBRE :( \n ';
+    //escribirArchivo('./ejemplo.txt', '');
+    //console.log(textoleido + nuevoContenido);
 
